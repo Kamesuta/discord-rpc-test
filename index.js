b0169ab81056ab65c353749ff1d89e9f2c834461
Kamesuta <kamesuta@gmail.com>
2024-02-18 04:18:54 +0900

    複数クライアント対応させようとした

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const localStorage = new LocalStorage('./saves');
 
 // Discord RPC Client の初期化
 const client = new Client({
-    transport: 'ipc',
+    transport: 'websocket',
     origin: 'https://streamkit.discord.com',
 });
 
@@ -72,27 +72,101 @@ client.on('ready', async () => {
     const vc = await client.request('GET_SELECTED_VOICE_CHANNEL');
     console.log(vc);
 
-    client.setUserVoiceSettings('655572647777796097', {
-        volume: 0.5,
-    });
+    // client.setUserVoiceSettings('655572647777796097', {
+    //     volume: 0.5,
+    //     pan: {
+    //         left: 0.9,
+    //         right: 0.1,
+    //     }
+    // });
 });
 
-try {
-    // アクセストークンが保存されている場合は、それを利用してログインする
-    const accessToken = localStorage.getItem('accessToken');
-    if (accessToken) {
+/**
+ * Discord/Discord PTB/Discord Canary/Discord Developmentにログインして、入出中のボイスチャンネルを取得します。
+ * ボイスチャンネルに入っているクライアントがいない場合は、nullを返します。
+ */
+async function loginAndGetVoiceChannel(accessToken, tries = 0) {
+    try {
+        // TransportがWebSocketかIPCかを判定する
+        if (client.transport.tries !== undefined) {
+            // 接続を試みる
+            await new Promise((resolve, reject) => {
+                // WebSocketの場合
+                client.transport.tries = tries + 20; // 20以上に設定することで、内部で再接続を試みないようにする
+                // 20以上に設定することで、必ずエラーが発生するため、エラー時の処理が実行される
+                client.transport.on('error', () => reject(new Error('クライアントが見つかりません (WebSocket)')));
+                // 接続を試みる
+                client.connect(clientId)
+                    .then(() => resolve())
+                    .catch(() => reject(new Error('接続に失敗しました (WebSocket)')));
+            });
+        } else {
+            // IPCの場合
+            const endpoint = `http://127.0.0.1:${6463 + (tries % 10)}`;
+            const success = await fetch(endpoint)
+                .then((r) => r.status === 404)
+                .catch(() => false);
+
+            if (!success) {
+                throw new Error('接続に失敗しました (IPC)');
+            }
+        }
+
+        // Discord/Discord PTB/Discord Canary/Discord Developmentのどれかにログインする
         await client.login({
             clientId,
             scopes,
             accessToken,
             redirectUri: 'https://streamkit.discord.com/',
-        });
+        }).catch(() => new Error('ログインに失敗しました'));
+
+        // ボイスチャンネルに接続する
+        const vc = await client.request('GET_SELECTED_VOICE_CHANNEL');
+
+        if (vc) {
+            // ボイスチャンネルに接続できた場合は、そのボイスチャンネルを返す
+            return vc;
+        } else {
+            // ボイスチャンネルに接続できなかった場合は、次のポートを再試行する
+            console.warn('接続したDiscordクライアントはボイスチャンネルに接続していません。');
+        }
+    } catch (error) {
+        // ログインに失敗した場合は、次のポートを再試行する
+        console.error(error.message);
+    }
+
+    // ポートを変えて再試行する
+    if (tries >= 10) {
+        // ボイスチャンネルに接続できなかった場合は、エラーを投げる
+        throw new Error('すべてのDiscordクライアントがボイスチャンネルに接続していません');
+    }
+
+    // ボイスチャンネルに接続できなかった場合は、次のポートを再試行する
+    console.warn(`次のポートを試します。(${tries + 1}回目)`);
+
+    // Clientをリセットする
+    try {
+        await client.destroy();
+        client._connectPromise = undefined;
+    } catch (error) {
+        throw new Error('クライアントの破棄に失敗しました');
+    }
+
+    // ポートを変えて再試行する
+    return await loginAndGetVoiceChannel(accessToken, tries + 1);
+}
+
+try {
+    // アクセストークンが保存されている場合は、それを利用してログインする
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+        await loginAndGetVoiceChannel(accessToken);
     } else {
-        throw new Error('Access token not found');
+        throw new Error('アクセストークンが見つかりません');
     }
 } catch (error) {
     // ログ出力
-    console.error('Failed to login with access token, trying to login without access token');
+    console.error('アクセストークンでのログインに失敗しました。アクセストークンなしでログインを試みます。');
 
     // ログインに失敗した場合は、アクセストークンなしでログインする
     await client.login({
